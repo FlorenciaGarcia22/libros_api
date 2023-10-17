526e3dbe3be00c92285624ffec8156e79e7263d5
FlorenciaGarcia <garciaflorencia897@gmail.com>
2023-10-17 12:30:38 -0300

    Api rest Libros

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import {Router} from 'express';
+
+import {libros} from './controller.js';
+
+export const router = Router()
+
+router.get('/libros',libros.getAll);
+router.get('/libros',libros.getOne);
