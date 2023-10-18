eb1d82af667012e7da60284dbbd8520931a45848
FlorenciaGarcia <garciaflorencia897@gmail.com>
2023-10-18 18:45:50 -0300

    correccion id

--- src/controller.js
+++ src/controller.js
@@ -6,11 +6,17 @@ class LibroController{
         res.json(result);
     }
     async getOne(req, res){
-        const libros = req.body;
-        const [result] = await pool.query(`SELECT * FROM libros WHERE id = ?`, [libros.id]);
-        res.json(result);
+        const id = req.body.id;
+        const [result] = await pool.query(`SELECT * FROM libros WHERE id = ?`, [id]);
+       if (result.length>0) {
+            res.json(result[0]);
+        } else {
+            res.status(404).json ({"error":`no se encontro el libro id: ${id}`});
+        }
+       
     }
-
 }
+
+
 
 export const libros = new LibroController();
