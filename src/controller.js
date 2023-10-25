066b262e89e6671e27580b058e8284ff45ba3ca6
FlorenciaGarcia <garciaflorencia897@gmail.com>
2023-10-24 21:56:03 -0300

    incluir try y catch

--- src/controller.js
+++ src/controller.js
@@ -6,15 +6,53 @@ class LibroController{
         res.json(result);
     }
     async getOne(req, res){
+        try{
         const id = req.body.id;
         const [result] = await pool.query(`SELECT * FROM libros WHERE id = ?`, [id]);
-       if (result.length>0) {
-            res.json(result[0]);
-        } else {
-            res.status(404).json ({"error":`no se encontro el libro id: ${id}`});
-        }
-       
+       if (result.length==0) {
+            throw new error('libro no encontrado.');
+        } res.json(result[0]); 
+    }catch (error){
+        console.log(error);
+        res.status(404).json({error: 'id inexistente.'});
     }
+          
+    }
+
+    async add(req, res){
+        try{
+        const libro = req.body;
+        const [result] = await pool.query('INSERT INTO libros (nombre, autor, categoria, año_publicacion, ISBN) VALUES (?,?,?,?,?)',[libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
+        res.json({"id insertado": result.insertId});
+        }catch (error){
+            console.log(error);
+            res.status(404).json({error:'Error al agregar libro, verifique los datos ingresados.'});
+        }
+    }
+
+    async delete(req, res){
+        try{
+        const libro = req.body;
+        const [result] = await pool.query('DELETE FROM libros WHERE ISBN=(?)',[libro.ISBN]);
+        res.json({"Registros Eliminado": result.affectedRows});
+    }catch (error){
+        console.log(error);
+        res.status(404).json({error: 'Error al eliminar libro ISNB inexistente.'});
+    }
+    }
+
+    async update(req, res){
+        try{
+        const libro = req.body;
+        const [result] = await pool.query(`UPDATE libros SET nombre=(?),autor=(?),categoria=(?),año_publicacion=(?),ISBN=(?) WHERE id=(?)`,[libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN, libro.id]);
+        res.json({"Registros Actualizados": result.changedRows});
+        }catch(error){
+            console.log(error);
+            res.status(500).json({error:'Error al actualizar libro.'});
+        }
+    }
+
+
 }
 
 
--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,7 @@ import {libros} from './controller.js';
 export const router = Router()
 
 router.get('/libros',libros.getAll);
-router.get('/libros',libros.getOne);
+router.get('/libroId',libros.getOne);
+router.post('/agregarLibro',libros.add);
+router.delete('/eliminarLibro',libros.delete);
+router.put('/actualizarLibro',libros.update);
